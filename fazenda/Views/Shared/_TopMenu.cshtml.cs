23d73b96be4956f8bb8b6f8b0ec321a3733b43d9
Bruno <bruno.ribeiro@itpower.co>
2024-11-06 14:04:16 -0300

    teste

---
+++ fazenda/Views/Shared/_TopMenu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace fazenda.Views.Shared
+{
+    public class _TopMenuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
