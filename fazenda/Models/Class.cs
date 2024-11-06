93b1541bf90730438510d123b25bdb421dc24d10
Bruno <bruno.ribeiro@itpower.co>
2024-11-06 13:44:44 -0300

    Adicionar arquivos de projeto.

---
+++ fazenda/Models/Class.cs
@@ -0,0 +1,13 @@
+﻿namespace fazenda.Models
+{
+    public class User
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ fazenda/Views/Account/login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace fazenda.Views.Account
+{
+    public class loginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
